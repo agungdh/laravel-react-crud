bd8f354a4957a1b7a2dcd1bc88e0b5bf9e8bd965
AgungDH <agungdh@live.com>
2025-10-07 10:35:34 +0700

    chore(uwong): clean up unused import and minor formatting fix
    - Removed unused `Str` import from Uwong model
    - Minor formatting adjustment in UwongController for cleaner code

--- app/Http/Controllers/UwongController.php
+++ app/Http/Controllers/UwongController.php
@@ -26,5 +26,4 @@ class UwongController extends Controller
     {
         return $uwong;
     }
-
 }
--- app/Models/Uwong.php
+++ app/Models/Uwong.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Uwong extends Model
 {
